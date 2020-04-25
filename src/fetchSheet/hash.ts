2f558311c304a7712ab68117e8f061e8ff059f29
butlerx <butlerx@notthe.cloud>
2020-04-25 19:53:28 +0100

    import uuidv5 correctly

--- src/fetchSheet/hash.ts
+++ src/fetchSheet/hash.ts
@@ -1,4 +1,4 @@
-import uuidv5 from 'uuid/v5';
+import { v5 as uuidv5 } from 'uuid';
 
 const seedConstant = uuidv5('gsheet', '2972963f-2fcf-4567-9237-c09a2b436541');
 export const hash = (str: string) => uuidv5(str, seedConstant);
